b6516b87d4b7cf0eba9e873ab371159df25f879c
Chloe905 <cchloehsieh@gmail.com>
2024-04-09 17:59:42 +0800

    add signIn & signUp page

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,24 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import SignIn from '../views/SignIn.vue'
 import NotFound from '../views/NotFound.vue'
 
 const routes = [
+  {
+    path: '/signin',
+    name: 'sign-in',
+    component: SignIn
+  },
+  {
+    path: '/signup',
+    name: 'sign-up',
+    component: () => import('../views/SignUp.vue')
+  },
   {
     // path: '*', used in vue router 3 
     path: '/:catchAll(.*)',
     name: 'not-found',
     component: NotFound
-  },
+  }
 ]
 
 const router = createRouter({
